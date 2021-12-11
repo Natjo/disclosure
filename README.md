6ab599a347687a09885f6674c72333e59a085d8c
Jonathan MARTIN <j.martin@lonsdale.fr>
2021-12-11 17:08:11 +0100

    first

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# disclosure
+
+# Disclosure
+
+![version](https://img.shields.io/github/manifest-json/v/Natjo/disclosure)
+
+Accesible disclore component to hide/show panel.
+
+btn must have the id of panel in aria-control
+
+## Parameters
+| Name | Arguments | Description |
+| ------ | ------ | ------ |
+| el | htmlElement | element that contain btn and panel |
+| btn | selector | |
+| onopen | function | fired on open |
+| onclose | function | fired on close |
+| autoclose | function | et if panel stay open on focuse out |
+
+## Usage
+
+### html
+```html
+<div class="dropdown">
+	<button class="summary" 
+	aria-expanded="false" 
+	aria-controls="mypanel" 
+	aria-label="See specification">Specification</button>
+	<form class="panel" id="mypanel">
+	<h2>Specifications</h2>
+	<input type="checkbox"><label>Contrast</label>
+	</form>
+</div>
+```
+### javascript
+```javascript
+import disclosure from './modules/disclosure/disclosure';
+disclosure({
+	el: dropdown,
+	btn: '.summary',
+	onopen(){
+	
+	}
+})
+```
+
+## Demo
+[See codepen demo](https://codepen.io/natjo/pen/oNGzJGK?editors=0011)
---
+++ disclosure.js
@@ -0,0 +1,47 @@
+/**
+ * Disclosure
+ * 
+ * hide/show panel
+ * btn must have the id of panel in aria-control
+ * 
+ * @param {el} htmlElement  element that contain btn and panel
+ * @param {btn} selector 
+ * @param {onclose} function fired when panel is closing
+ * @param {onopen} function fired when panel is opening
+ * @param {autoclose} boolean set if panel stay open on focuse out
+ * 
+ */
+
+ function disclosure(params) {
+	const el = params.el;
+	const btn = el.querySelector(params.btn || 'summary');
+	const clicktouch = "ontouchstart" in document.documentElement ? 'touchstart' : 'click';
+	const clickOut = (e) => !el.contains(e.target) && close();
+	const keyup = (e) => {
+		!el.contains(document.activeElement) && close();
+		if(e.key === 'Escape'){
+			btn.focus();
+			close();
+		}
+	};
+	const open = () => {
+		btn.setAttribute('aria-expanded', true);
+		document.addEventListener('keyup', keyup);
+		window.addEventListener(clicktouch, clickOut);
+		if (typeof params.onopen === 'function') params.onopen(el);
+	};
+	const close = () => {
+		if(params.autoclose !== false){
+			btn.setAttribute('aria-expanded', false);
+			document.removeEventListener('keyup', keyup);
+			window.removeEventListener(clicktouch, clickOut);
+			if (typeof params.onclose === 'function') params.onclose(el);
+		}
+	};
+	btn.onclick = (e) => {
+		e.preventDefault();
+		btn.getAttribute('aria-expanded') === 'true' ? close() : open();
+	};
+}
+
+export default disclosure;
